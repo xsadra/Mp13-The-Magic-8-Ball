b3d61d72e0c23fc857a14ff0e8a295e382958b09
Sadra <ti9mail@gmail.com>
2018-04-16 15:26:48 +0200

    Add Spring structure

---
+++ src/main/java/de/sadrab/Mp13TheMagic8Ball/Mp13TheMagic8BallApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.Mp13TheMagic8Ball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp13TheMagic8BallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp13TheMagic8BallApplication.class, args);
+	}
+}
---
+++ src/test/java/de/sadrab/Mp13TheMagic8Ball/Mp13TheMagic8BallApplicationTests.java
@@ -0,0 +1,16 @@
+package de.sadrab.Mp13TheMagic8Ball;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp13TheMagic8BallApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
